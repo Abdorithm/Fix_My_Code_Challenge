cc19f4fdb8299aba9c2ec785280d7900d2f7932a
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-01-30 20:46:12 +0200

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,61 +1,31 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""
+Square class
+"""
 
+class square():
+    """ Documentation"""
 
-class square:
-    """Represents a square
-    Attributes:
-        __size (int): size of a side of the square
-    """
-    def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-            None
-        """
-        self.size = size
+    def __init__(self, *args, **kwargs):
+        """ Documentation """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.__size * self.__size
+        return self.width * self.width
 
     def PermiterOfMySquare(self):
-        """ Permiter of the square """
-        return (self.__size * 4)
-
-    @property
-    def size(self):
-        """getter of __size
-        Returns:
-            The size of the square
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """setter of __size
-        Args:
-            value (int): the size of a size of the square
-        Returns:
-            None
-        """
-        if type(value) is not int:
-            raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        """ Documentation """
+        return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
         """ Documentation """
-        return "{}/{}".format(self.__size, self.__size)
-
+        return "{}/{}".format(self.width, self.width)
 
 if __name__ == "__main__":
 
-    s = square(size=12)
+    s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
