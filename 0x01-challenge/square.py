ce5d9f1c02946f078991010b6ce2be97bc0b5dbd
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-01-30 20:44:02 +0200

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,27 +1,56 @@
 #!/usr/bin/python3
-"""
-Square class
-"""
+"""Defines a class Square"""
 
 
-class square():
-    """ Documentation"""
-
+class square:
+    """Represents a square
+    Attributes:
+        __size (int): size of a side of the square
+    """
     def __init__(self, size=0):
-        """ Documentation """
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
         self.size = size
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.size ** 2
+        return self.__size * self.__size
 
     def PermiterOfMySquare(self):
-        """ Documentation """
-        return (self.size * 4)
+        """ Permiter of the square """
+        return (self.__size * 4)
+
+    @property
+    def size(self):
+        """getter of __size
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter of __size
+        Args:
+            value (int): the size of a size of the square
+        Returns:
+            None
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
 
     def __str__(self):
         """ Documentation """
-        return "{}/{}".format(self.size, self.size)
+        return "{}/{}".format(self.__size, self.__size)
 
 
 if __name__ == "__main__":
