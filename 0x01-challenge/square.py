7cf1d7e79f84446ead7486807ccbef94bbc9ead2
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-01-30 20:20:47 +0200

    fix squares

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,27 +7,26 @@ Square class
 class square():
     """ Documentation"""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, size=0):
         """ Documentation """
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+        self.size = size
 
     def area_of_my_square(self):
         """ Area of the square """
-        return self.width * self.width
+        return self.size ** 2
 
     def PermiterOfMySquare(self):
         """ Documentation """
-        return (self.width * 4)
+        return (self.size * 4)
 
     def __str__(self):
         """ Documentation """
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.size, self.size)
 
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(size=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
