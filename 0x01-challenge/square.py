a2550fb0062dd1c58662af69cf08291530f7c1da
Abdelrahman Magdi <abdomagdi300@gmail.com>
2024-01-30 20:17:09 +0200

    fix square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
+"""
+Square class
+"""
+
 
 class square():
-    
-    width = 0
-    height = 0
+    """ Documentation"""
 
-    
     def __init__(self, *args, **kwargs):
+        """ Documentation """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -15,10 +17,13 @@ class square():
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Documentation """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Documentation """
         return "{}/{}".format(self.width, self.height)
+
 
 if __name__ == "__main__":
 
